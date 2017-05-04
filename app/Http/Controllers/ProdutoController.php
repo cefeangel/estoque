5a818888fe0602e1f89deb96b24fe219e34ef119
cefeangel <cefeangel@hotmail.com>
2017-05-04 13:17:09 -0300

    Projeto estoque Laravel Insert ProjetoController e Model Produto

--- app/Http/Controllers/ProdutoController.php
+++ app/Http/Controllers/ProdutoController.php
@@ -9,10 +9,18 @@ class ProdutoController extends Controller
 {
     
     public function lista(){
-                    //guery builder
+                    //query builder
         $produtos = DB::table('produtos')->get();
         
         return view('listagem')->with('produtos', $produtos);
+    }
+    
+    public function mostra(){
+        //query builder
+        $produto = DB::table('produtos')
+                ->where('id','=','3')
+                ->get();
+        return view('detalhe')->with('p', $produto[0]);
     }
     
 }
--- resources/views/listagem.php
+++ resources/views/listagem.php
@@ -1,17 +1,27 @@
 <html>
-    <head></head>
+    <head>
+        <link href="/css/app.css" rel="stylesheet" >
+        <title>Controle de estoque</title>
+    </head>
     <body>
-        <h1>Listagem Produtos</h1>
-        <table>
-            <?php foreach ($produtos as $key => $p): ?>
-            <tr>
-                <td><?php echo $p->nome; ?></td>
-                <td><?php echo $p->valor; ?></td>
-                <td><?php echo $p->descricao; ?></td>
-                <td><?php echo $p->quantidade; ?></td>
-            </tr>
-            <?php endforeach; ?>
-        </table>
+        <div class="container">
+            <h1>Listagem Produtos</h1>
+            <table class="table table-striped table-bordered table-hover">
+
+                <?php foreach ($produtos as $key => $p): ?>
+                    <tr>
+                        <td><?php echo $p->nome; ?></td>
+                        <td><?php echo $p->valor; ?></td>
+                        <td><?php echo $p->descricao; ?></td>
+                        <td><?php echo $p->quantidade; ?></td>
+                        <td>
+                            <a href="/produtos/mostra" >Visualizar
+                            </a>
+                        </td>
+                    </tr>
+                <?php endforeach; ?>
+            </table>
+        </div>
     </body>
 </html>
 
